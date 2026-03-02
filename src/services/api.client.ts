96af1e79c2cd847ad9d5e08a864fe9b83da71a06
Sara <sarachavez500@gmail.com>
2026-03-02 16:12:32 -0500

    feat: auth store, API client y hooks de TanStack Query

---
+++ src/services/api.client.ts
@@ -0,0 +1,29 @@
+import axios from 'axios'
+import { API_URL } from '@constants/api.constants'
+import { ROUTES } from '@constants/routes.constants'
+import { useAuthStore } from '@store/auth.store'
+
+export const apiClient = axios.create({
+  baseURL: API_URL,
+  headers: { 'Content-Type': 'application/json' },
+  timeout: 10000,
+})
+
+apiClient.interceptors.request.use((config) => {
+  const token = useAuthStore.getState().token
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
+apiClient.interceptors.response.use(
+  (response) => response,
+  (error: unknown) => {
+    if (axios.isAxiosError(error) && error.response?.status === 401) {
+      useAuthStore.getState().clearAuth()
+      window.location.href = ROUTES.LOGIN
+    }
+    return Promise.reject(error)
+  }
+)
