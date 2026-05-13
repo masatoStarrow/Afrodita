763023faa059e666bafd57d80ed924e5fb3b1651
dishyma <dishyma@proton.me>
2026-05-13 16:02:21 -0500

    fix: disable react-refresh lint rule for FilterPanel constants export

--- src/components/organisms/FilterPanel/FilterPanel.tsx
+++ src/components/organisms/FilterPanel/FilterPanel.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 import { useCallback, useEffect } from 'react'
 import type { InteractionType, InteractionStatus } from '@app-types/interaction.types'
 import styles from './FilterPanel.module.css'
